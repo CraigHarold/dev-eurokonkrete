fa8c2d3213681313110f8d76978d89da9a9e1b03
Anurag Gupta <angupta@innominds.com>
2021-06-16 16:16:30 +0530

    dev files

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -6,39 +6,48 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-  headers = [
-      {
-          "name": "Home",
-          "url": "/",
-          "sequence": 1
-      },
-      {
-          "name": "About",
-          "url": "",
-          "sequence": 2,
-          "subChild": [
-              {
-                  "name": "Pudlo",
-                  "url": "/about",
-              }
-          ]
-      },
-      {
-          "name": "Downloads",
-          "url": "/downloads",
-          "sequence": 3
-      },
-      {
-          "name": "Contact",
-          "url": "/contact",
-          "sequence": 4
-      }
-  ];
+    showProductDropdown: boolean;
+    headers = [
+        {
+            "name": "Home",
+            "url": "/",
+            "sequence": 1
+        },
+        {
+            "name": "About",
+            "url": "",
+            "sequence": 2,
+            "subChild": [
+                {
+                    "name": "Pudlo",
+                    "url": "/about",
+                }
+            ]
+        },
+        {
+            "name": "Downloads",
+            "url": "/downloads",
+            "sequence": 3
+        },
+        {
+            "name": "Contact",
+            "url": "/contact",
+            "sequence": 4
+        }
+    ];
   constructor() { }
 
   @Input() className: string;
 
   ngOnInit(): void {
+  }
+
+  dropdownClick(id) {
+      switch (id) {
+          case "products" : 
+          this.showProductDropdown = !this.showProductDropdown
+          break;
+      }
   }
 
 }
